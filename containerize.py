597496ea201f09dcae397968e00cfaa012235314
Phil Wilson <pgw@acm.org>
2020-03-26 19:48:52 +0000

    Actually exec command line

--- containerize.py
+++ containerize.py
@@ -8,9 +8,38 @@
 import os, sys
 
 
-def run():
-    print("running ", sys.argv[2:])
+def lex(text):
+    return text.rstrip().split()
 
+def resolve_path(progname):
+    for directory in os.environ["PATH"].split(':'):
+        testpath = '/'.join([directory, progname])
+        if os.path.isfile(testpath):
+            return testpath
+
+    return None
+
+
+def run():
+    path = resolve_path(sys.argv[2])
+    if path:
+        pid = os.fork()
+        if pid == 0:
+            os.execv(path, sys.argv[2:])
+        else:
+            status = os.wait()
+    else:
+        sys.stdout.write("command not found\n")
+
+    
+#    print("running ", sys.argv[2:])
+
+
+
+
+
+
+    
 def main():
     if sys.argv[1] == 'run':
         run()
